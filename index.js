f430af952d2eca9b14e6f3a24b16e21b622ae2f7
haze <isnt@haze.cool>
2020-10-01 09:58:37 -0400

    Update index.js
    Allow cli arg to take in file rather than reading from a static one

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let src = fs.readFileSync('code.java').toString();
+let src = fs.readFileSync(process.argv[1]).toString();
 
 src = src.replace(/(\r\n|\n|\r)/gm,"");
 src = src.replace("String[] args", "String[]a")
